165210a9e84a9a6751fca4d399755eb5d36ae0c2
SamvsP4K <soundsofsamvs@gmail.com>
2022-10-22 17:36:50 -0700

    final edits made

--- Assignment_Data/crowdfunding_SQL_Analysis.sql
+++ Assignment_Data/crowdfunding_SQL_Analysis.sql
@@ -11,22 +11,6 @@ GROUP BY cf_id
 ORDER BY backers_count DESC;
 
 
-
---examples
-SELECT COUNT(ce.emp_no), de.dept_no
-FROM current_emp as ce
-LEFT JOIN dept_emp as de
-ON ce.emp_no = de.emp_no
-GROUP BY de.dept_no;
-
-SELECT COUNT(ce.emp_no), de.dept_no
-FROM current_emp as ce
-LEFT JOIN dept_emp as de
-ON ce.emp_no = de.emp_no
-GROUP BY de.dept_no
-ORDER BY de.dept_no;
-
-
 -- 2. (2.5 pts)
 -- Using the "backers" table confirm the results in the first query.
 
